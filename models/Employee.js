18b2154049c982f6dfe2d1e9843f7ccb4c36ec5d
Rafael Nonato <rafaelnonato79@gmail.com>
2025-01-13 19:53:55 -0300

    Alterado campos do bd

--- models/Employee.js
+++ models/Employee.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const EmployeeSchema = new mongoose.Schema({
+    foto: { type: String, required: true }, // URL da foto do funcionário
     cargo: { type: String, required: true },
     data_ingresso: { type: Date, required: true },
     issues: { type: Number, required: true },
@@ -10,11 +11,12 @@ const EmployeeSchema = new mongoose.Schema({
     tempo_de_empresa_em_meses: { type: Number, required: true },
     timeline: [
         {
-            date: { type: String, required: true },
+            date: { type: String, required: true }, // Formato de data como string (ex: "2024/07")
             titulo: { type: String, required: true },
             descricao: { type: String, required: true }
         }
-    ]
+    ],
+    junior: { type: Boolean, required: true } // Indica se é um funcionário júnior
 });
 
 module.exports = mongoose.model('Employee', EmployeeSchema);
